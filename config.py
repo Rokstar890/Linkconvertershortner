a14dc3eb3ea4374e21356ebe942fdd78bdd9eee8
TECHNICAL RB <121280660+TECHNICAL-RB@users.noreply.github.com>
2023-06-27 18:12:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "21114403"))
+API_HASH = os.environ.get("API_HASH", "192c2b697e9f45650354fd47292ccf78")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5870227633:AAEi2mNjiaWuo0vYRta-XHEQ4zhhMKJI1CI")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("6201745002")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "telegramms")
 DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5864846606")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(1255023013)
 
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001646129271")) 
 
 # For Force Subscription
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @")
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ziplinker_net")
 
 # true if forward should be avoided
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True")
