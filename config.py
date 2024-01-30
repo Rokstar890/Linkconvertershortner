1875e7e6e8503a1225e3fe2949aeda0f8040952b
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-30 23:38:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "21114403"))
-API_HASH = os.environ.get("API_HASH", "192c2b697e9f45650354fd47292ccf78")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5870227633:AAH_9DYDYaNBCi4S3e8MnGHArjACi57cdbA")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("6201745002")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "24003416"))
+API_HASH = os.environ.get("API_HASH", "013d9b3fe388308ff1272d33d1e40e2b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("6683865620")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "telegramms")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://hegodal811:3ppOUsXHwucY3zC4@cluster0.xlbxgdr.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "5864846606")) 
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://pispibordu:Rokstar321@cluster0.roeprbx.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "6683865620")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(1255023013)
 
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001646129271")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002133531200")) 
 
 # For Force Subscription
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ziplinker_net")
